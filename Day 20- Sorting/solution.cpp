a4ad229c73ef44314dc1c9bd77fbb6a9544c727a
npapadimitriou <npapadimitriou@users.noreply.github.com>
2018-12-11 00:35:06 +0100

    solution.cpp

---
+++ Day 20- Sorting/solution.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int sorting(vector<int>*);
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int a_i = 0; a_i < n; a_i++){
+    	cin >> a[a_i];
+    }
+  int swaps = sorting(&a);
+
+    cout<<"Array is sorted in "<<swaps<<" swaps."<<endl;
+    cout<<"First Element: "<<a.front()<<endl;
+    cout<<"Last Element: "<<a.back()<<endl;
+
+    return 0;
+}
+
+int sorting(vector<int>* a){
+    int numberOfSwaps = 0;
+    int n= (*a).size();
+    for (int i = 0; i < n; i++) {
+        // Track number of elements swapped during a single array traversal
+        
+        
+        for (int j = 0; j < n - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order
+            if ((*a)[j] > (*a)[j + 1]) {
+                swap((*a)[j], (*a)[j + 1]);
+                numberOfSwaps++;
+            }
+        }
+        
+        // If no elements were swapped during a traversal, array is sorted
+        if (numberOfSwaps == 0) {
+            break;
+        }
+    }   
+    return numberOfSwaps;
+
+}
+
